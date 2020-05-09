ca0cc0aa63102a0048cbdc539500dc90641c04c5
CaseyPYZ <casey.yzpan@gmail.com>
2020-05-09 20:36:43 +0800

    socket2 update

---
+++ socket2/client2_receive.js
@@ -0,0 +1,75 @@
+var io = require('socket.io-client');
+const rs2 = require("node-librealsense")
+// console.log(rs2);
+const {GLFWWindow} = require('./glfw-window.js');
+const {glfw} = require('./glfw-window.js');
+
+const orle = require('orle');
+
+const socket = io.connect('http://localhost:3001');
+socket.on('news', (data) => {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});
+
+var file = "haha";
+socket.emit('file_transfer',file);
+
+const win = new GLFWWindow(800, 450, 'Bag Streaming Test');
+const colorizer = new rs2.Colorizer();
+
+var count = 0;
+let specs = {
+  width: 0,
+  height: 0
+};
+
+socket.on('meta',(data) => {
+  specs.width = data.width;
+  specs.height = data.height;
+});
+
+
+socket.on('file_receive',(data)=>{
+  receive(data);
+});
+
+socket.on('sender_done',(data)=>{
+  console.log(data);
+  win.destroy();
+});
+
+
+/**
+ * async Receive function
+ * @param {ArrayBuffer} data
+ */
+async function receive(data){
+
+  /*** ORLE DECODING ***
+     * const depthMap = await orle.decode(data);
+     */
+  
+  const depthMap = colorizer.colorize(data);
+
+  win.beginPaint();
+  // const color = frameset.colorFrame;
+  // console.log(color.width);
+  // console.log(color.height);
+  glfw.draw2x2Streams(win.window, 1,
+      depthMap.data, 'rgb8', specs.width, specs.height,
+      // color.data, 'rgb8', color.width, color.height
+      );
+
+  win.endPaint();
+
+  count++;
+  console.log(count);
+  console.log(Date.now());
+
+  // If window is closed
+  if( win.shouldWindowClose() ){
+    // END STREAMING
+    socket.emit('receiver_break',{msg : "Receiver ended stream."});
+  }
+}
---
+++ socket2/client2_send.js
@@ -0,0 +1,110 @@
+const io = require('socket.io-client');
+const rs2 = require("node-librealsense")
+const {GLFWWindow} = require('./glfw-window.js');
+const {glfw} = require('./glfw-window.js');
+
+const orle = require('orle');
+
+const { createImageData } = require('canvas');
+
+// A GLFW Window to display the captured image
+const win = new GLFWWindow(1280, 720, 'Node.js Capture Example');
+
+// Colorizer is used to map distance in depth image into different colors
+const colorizer = new rs2.Colorizer();
+
+//const file = '/Users/michaelzhang/Desktop/CS_Capstone/test_files/d435i_walking.bag';
+const file = '/Users/user/Desktop/CS_Capstone/test_files/d435i_walking.bag';
+
+let cfg = new rs2.Config();
+cfg.enableDeviceFromFile(file);
+let pipeline = new rs2.Pipeline();
+pipeline.start(cfg);
+
+let lookahead = false;
+let streamOn = true;
+
+const socket = io.connect('http://localhost:3001');
+socket.on('news', (data) => {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});
+
+
+// socket.on('file_receive',(data)=>{
+//   console.log(data);
+// });
+
+socket.on('receiver_break',(data)=>{
+  console.log(data.msg);
+  streamOn = false;
+});
+
+const judge = true;
+// let frameset = pipeline.waitForFrames();
+// let depthMap = colorizer.colorize(frameset.depthFrame);
+// setInterval(() => {
+    
+//     socket.emit('file_transfer',"balabala3");
+//     console.log("depthMap");
+//     // frameset = pipeline.waitForFrames();
+//     // depthMap = colorizer.colorize(frameset.depthFrame);
+    
+// }, 1000/30);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Transfer data
+ */
+async function trans() {
+
+    let count = 0;
+    while(streamOn){
+      //const frameset = pipeline.waitForFrames();
+      const frameset = pipeline.pollForFrames();
+      if(frameset){
+        //const depthMap = colorizer.colorize(frameset.depthFrame);
+
+        // Emit Metadata First
+        if(!lookahead){
+          let specifics = {
+            width: 1280,
+            height: 720
+          };
+          socket.emit('meta', specifics);
+          lookahead = true;
+        }
+
+        /*** ORLE ENCODING - much more delay ***
+           * const buffer = await orle.encode( depthMap.data );
+           * socket.emit('file_transfer', buffer);
+           */
+
+        //var imgData = createImageData(new Uint16Array(depthMap.data.buffer), depthMap.width, depthMap.height);
+
+        socket.emit('file_transfer', frameset.depthFrame );
+
+        await sleep(34);
+
+        console.log(Date.now());
+        count += 1;
+        console.log(count);
+
+        if(count>=100){
+          console.log("Ending.");
+          break;
+        }
+      }
+    }
+
+    socket.emit('sender_done',"Sender done.");
+    pipeline.stop();
+    pipeline.destroy();
+    win.destroy();
+    rs2.cleanup();
+}
+
+trans()
